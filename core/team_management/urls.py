b96ba024b38ceaac87221aabfe5a127bca186f30
Jaideep Kular <kularjaideep@gmail.com>
2023-12-08 08:16:37 -0500

    Refactored views using generic views

--- core/team_management/urls.py
+++ core/team_management/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.MemberList.as_view(), name="members"),
     path('addMember', views.MemberAdd.as_view(), name="add-member"),
-    path('editMember/<str:id>', views.MemberEdit.as_view(), name="edit-member"),
-    path('deleteMember/<str:id>', views.MemberDelete.as_view(), name="delete-member"),
+    path('editMember/<pk>', views.MemberEdit.as_view(), name="edit-member"),
+    path('deleteMember/<pk>', views.MemberDelete.as_view(), name="delete-member"),
 ]
--- core/team_management/views.py
+++ core/team_management/views.py
@@ -1,50 +1,27 @@
 from django.shortcuts import render, redirect, reverse
 from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import User
-from .forms import UserForm
 
 # Create your views here.
 
-class MemberList(View):
-    def get(self, request):
-        users = User.objects.all()
-        context = {'members':users, 'num_members': len(users)}
-        return render(request, 'list.html', context)
+class MemberList(ListView):
+    model = User
+    template_name = 'list.html'
 
-class MemberAdd(View):
-    def get(self, request):
-        form = UserForm
-        return render(request, 'add.html', {'form': form})
-    
-    def post(self, request):
-        user_form = UserForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect(reverse('members'))
-        else:
-            return render(request, 'add.html', {'form': user_form})
+class MemberAdd(CreateView):
+    model = User
+    fields = '__all__'
+    template_name = 'add.html'
+    success_url = '/'
 
-class MemberEdit(View):
-    def get(self, request, id):
-        user = User.objects.get(id=id)
-        form = UserForm(instance=user)
-        context = {'form':form, 'user_id': id}
-        return render(request, 'edit.html', context)
+class MemberEdit(UpdateView):
+    model = User
+    fields = '__all__'
+    template_name = 'edit.html'
+    success_url = '/'
 
-    def post(self, request, id):
-        user = User.objects.get(id=id)
-        user_form = UserForm(request.POST, instance=user)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect(reverse('members'))
-        else:
-            return render(request, 'edit.html', {'form': user_form, 'user_id':id})
-
-class MemberDelete(View):
-    def get(self, request, id):
-        return redirect(reverse('members'))
-
-    def post(self, request, id):
-        user = User.objects.get(id=id)
-        user.delete()
-        return redirect(reverse('members'))
+class MemberDelete(DeleteView):
+    model = User
+    success_url = '/'
+    template_name = 'delete.html'
